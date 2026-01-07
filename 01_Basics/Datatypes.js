b27f65f649b54fc1fc73f4728e73766fdf1de157
Divyanshu Amit <dev.amyth27@gmail.com>
2026-01-07 07:23:07 +0000

    datatypes in javascript

---
+++ 01_Basics/Datatypes.js
@@ -0,0 +1,19 @@
+"user strict" // treat all javascript code as a new version
+//alert (3 + 3) // work on browser and we are working on node js (but will use in node js later)
+console.log("divyanshu") ; console.log("amit"); // avoid all these way of code, focus on code readability
+
+// datatypes : i) Numbers , ii) String. iii) Boolean, iv) Undefined
+
+let age = 12
+let nameStudent = "Divyanshu" // can use dobule quote or single quote for string
+let isLoggedIn = false
+let initialState;
+let currentScore = null; // null is a stand alone value;
+
+// type of used to identify the data type
+
+console.log(typeof 3) // number
+console.log(typeof 'Dev') // string
+console.log(typeof nameStudent) // string
+console.log(typeof initialState) // undefined
+console.log(typeof currentScore) // object
