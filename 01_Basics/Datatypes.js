ad319db432c4bf05f5e6d0d37c739bd6f18424b1
Divyanshu Amit <dev.amyth27@gmail.com>
2026-01-07 07:26:06 +0000

    datatypes in javascript

--- 01_Basics/Datatypes.js
+++ 01_Basics/Datatypes.js
@@ -10,6 +10,17 @@ let isLoggedIn = false
 let initialState;
 let currentScore = null; // null is a stand alone value;
 
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+
+// object
+
 // type of used to identify the data type
 
 console.log(typeof 3) // number
