1855e3a59fe2b19889c3925121479f820d388199
Divyanshu Amit <dev.amyth27@gmail.com>
2026-01-07 07:04:48 +0000

    learn about var, const, let

---
+++ 01_Basics/Variables.js
@@ -0,0 +1,16 @@
+// declaration of constants
+const studentId = 12345
+
+// declaration of variables
+let studentFirstName = "Angad"
+var studentLastName = "Singh"
+
+// another way of declaration
+studentEmail = "angadsingh234@co.in"
+
+
+/*
+const once declare cant be change.
+avoid using var due to functional and block scope issue
+*/
+console.table([studentId,studentFirstName,studentLastName, studentEmail])
