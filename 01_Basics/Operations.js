72fca873d21bc5166d0ec7f748c802b22048be51
Divyanshu Amit <dev.amyth27@gmail.com>
2026-01-08 02:54:12 +0000

    basic operations done

---
+++ 01_Basics/Operations.js
@@ -0,0 +1,42 @@
+// Basic operation same as other languages
+// console.log(2 + 2) 4
+// console.log(2 - 2) 0
+// console.log(2 * 2) 4
+// console.log(2 ** 3) 8 (power)
+// console.log(2 / 2) 1
+// console.log(2 % 3) 1 (reminder)
+
+// string operations
+
+let str1 = "hello"
+let str2 = " world"
+console.log(str1 + str2);
+
+// tricky operations
+
+console.log(1 + "2"); // 12
+console.log("1" + 2); //12
+console.log("1" + "2" + 3); //123
+console.log(1 + 2 + "3"); //33
+
+console.log(+true);  // 1  {transverse of this is not possible}
+console.log(+"") // 0 
+
+let num1, num2, num3 //prefer this 
+
+num1 = num2 = num3 = 2+2  // not this
+
+// increment
+
+let x = 5
+console.log(x++)
+console.log(x);
+
+let y = 8
+console.log(++y)
+console.log(y)
+
+
+
+
+
