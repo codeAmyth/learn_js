49cf328fb7f3ee8dd5faa1f63990050044e4086c
Divyanshu Amit <dev.amyth27@gmail.com>
2026-01-10 08:56:54 +0000

    array declarations, and basic functions done

---
+++ 02_basics/01_array.js
@@ -0,0 +1,56 @@
+// Declaration of Array
+
+const myArr = [0,1,2,3,4,5]
+let myArr2 = ["roman","seth","dean"]
+let myArr3 = new Array(6,7,8,9,10, "ankit", "rajat")
+// console.log(myArr3)
+
+/* Properties of Array : 
+
+i). 0 based indexing
+ii). includes different datatypes
+iii). JS arrays are resizeable
+iv). accessed by non negative integers only
+v). copy operations creates shallow copy rather than deep copies
+
+Shallow copy : A shallow copy of an object is a copy whose properties share the same references
+Deep Copies : A deep copy of an object is a copy whose properties do not share the same references
+*/
+
+// Arrays methods
+
+// myArr.push(7)
+// console.log(myArr);
+
+
+//  console.log(myArr.pop())
+//  console.log(myArr);
+ 
+
+//  myArr.unshift(8)
+// console.log(myArr);
+
+//  myArr.shift()
+//  console.log(myArr);
+
+//  let newArray = myArr.join()
+//  console.log(newArray)
+
+//  myArr2 = myArr3.join()
+//  console.log(typeof myArr2);
+
+// Slice and Splice
+
+// const nArr1 = myArr.slice(1,4)
+// console.log(nArr1);
+// console.log(myArr)
+
+// const nArr2 = myArr.splice(1,4)
+// console.log(nArr2)
+// console.log(myArr)
+
+ 
+
+
+
+
