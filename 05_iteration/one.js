ac89e480b519254742ba8ffb88f47015ad7370c3
Divyanshu Amit <dev.amyth27@gmail.com>
2026-01-15 14:51:36 +0000

    loops continues

---
+++ 05_iteration/one.js
@@ -0,0 +1,14 @@
+// for loop
+
+// for (let i = 0; i < array.length; i++) {
+//     // loop body
+    
+// }
+
+for (let i = 0; i <= 10; i++) {  
+    const element = i;
+    if (element === 5) {
+        console.log(`${element} is best number.`)
+    }
+    console.log(element)
+}
