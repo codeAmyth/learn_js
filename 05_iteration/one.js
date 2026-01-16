09c3b36f2069796e9fdaeaa7be84f9a61f4c136b
Divyanshu Amit <dev.amyth27@gmail.com>
2026-01-16 01:47:05 +0000

    for loop completed

--- 05_iteration/one.js
+++ 05_iteration/one.js
@@ -9,6 +9,44 @@ for (let i = 0; i <= 10; i++) {
     const element = i;
     if (element === 5) {
         console.log(`${element} is best number.`)
+     }
+    // console.log(element)
+}
+
+// nested loops
+
+// for (let i = 0; i <= 10; i++) {
+//     console.log(`Outer loop: ${i} is running`)
+//     for (let j = 0; j <= 10; j++) {
+//         console.log(`Inner loop: ${j} is running`)
+//     }
+// }
+
+// tables from 1 to 20
+// for (let i = 1; i < 11; i++) {
+//     console.log(`Table of ${i}`);
+//     for (let j = 1; j < 11; j++) {
+//         console.log(`${i} * ${j} = `+ i*j);
+//     }
+    
+// }
+
+// break
+
+for (let index = 0; index < 10; index++) {
+    
+    if (index == 5) {
+        console.log("5 is detected");
+        break;
     }
-    console.log(element)
+    console.log(index)
+}
+    
+for (let index = 0; index < 10; index++) {
+    
+    if (index == 5) {
+        console.log("5 is detected");
+        continue;
+    }
+    console.log(index)
 }
