6b3ab8d6926b3c1edc5b996800d07f6df4501b5d
Divyanshu Amit <dev.amyth27@gmail.com>
2026-01-16 01:59:25 +0000

    done with while and do while loop

---
+++ 05_iteration/two.js
@@ -0,0 +1,22 @@
+// while loop
+// let index = 0
+// while (index <= 10) {
+//     console.log(`Value is ${index}`);
+//     index++;
+// }
+
+// do-while
+
+// let score = 1;
+// do {
+//     console.log(score);
+//     score++
+// } while (score <= 10);
+
+// special case of do-while
+
+let score = 11;
+do {
+    console.log(score);
+    score++
+} while (score <= 10);
