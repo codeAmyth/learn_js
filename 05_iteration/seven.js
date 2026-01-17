77902c9d8bbf2eb85dd0220ee139e8bb597b47f0
Divyanshu Amit <dev.amyth27@gmail.com>
2026-01-17 09:32:42 +0000

    map and chaining

---
+++ 05_iteration/seven.js
@@ -0,0 +1,19 @@
+// map
+
+// task: take a array and add 10 to each number
+
+const myArr = [1,2,3,4,5,6,7,8,9,10]
+// const newArr = myArr.map( (num) => {
+//     return num + 10
+// })
+
+// console.log(myArr);
+// console.log(newArr);
+
+// chaining
+
+// const newArr = myArr.map( (num) => num * 10)
+//                     .map( (num) => num + 1)
+//                     .filter( (num) => num >=40) 
+// console.log(newArr)
+
