76c44457a0f44a74080c23f54df9789833527978
Divyanshu Amit <dev.amyth27@gmail.com>
2026-01-25 06:35:25 +0000

    completed

--- 08_advance_one/promises.js
+++ 08_advance_one/promises.js
@@ -66,3 +66,54 @@ promiseFour.then(function(user){
     console.log(error)
 })
 .finally(()=> console.log('The promise is either resolved or rejected'));
+
+
+// using await and async
+
+const promiseFive = new Promise( (resolve, reject) => {
+    setTimeout(() => {
+        let error = true;
+        if (!error) {
+            resolve({name : 'javascript' , extension : 'js'})
+        } else {
+            reject('Error: There is some error')
+        }
+    } , 1000)
+})
+
+async function consumePromiseFive() {
+    try {
+           const response = await promiseFive;
+           console.log(response);
+        
+    } catch (error) {
+        console.log(error);      
+    }  
+}
+
+consumePromiseFive()
+
+// async function getUsers() {
+
+//     try {
+//             const response = await fetch('https://jsonplaceholder.typicode.com/users')
+//             const data = await response.json();
+//             console.log(data);
+//     } catch (error) {
+//             console.log("E:", error);        
+//     }
+// }
+
+// getUsers();
+
+fetch('https://jsonplaceholder.typicode.com/users')
+.then( (response) => {
+    return response.json();
+})
+.then( (data) => {
+    console.log(data);
+})
+.catch((error)=> {
+    console.log(error);
+    
+})
